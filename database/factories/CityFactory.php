e406d31c6d8b37c43b8c536816c2800736d865a9
mimismosv <oicguevara@gmail.com>
2022-09-22 09:15:03 -0600

    factories

--- database/factories/CityFactory.php
+++ database/factories/CityFactory.php
@@ -17,7 +17,13 @@ class CityFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => fake()->city(),
+            'latitude' => fake()->latitude($min = -90, $max = 90),
+            'longitude' => fake()->longitude($min = -180, $max = 180),
+            'state_code' => fake()->stateAbbr(),
+            'state_name' => fake()->state(),
+            'country_code' => fake()->countryCode(),
+            'timezone' => fake()->timezone(),
         ];
     }
 }
